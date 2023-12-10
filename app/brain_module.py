4e701a2832bdb4c2d51ec812242285af3b5a820b
vamunarm <vamunarm@caracoltv.com.co>
2023-12-10 18:08:58 -0500

    add article generation

--- app/brain_module.py
+++ app/brain_module.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -9,23 +10,53 @@ class ChatGPT:
         self.client = OpenAI(
           api_key=os.getenv("OPENAI_API_KEY"),
         )
-        self.MAIN_ROLE = "This is the behavior of chatGPT"
+        self.SPANISH_LANGUAGE="es"
+        self.TRANSCRIBE_MODEL="whisper-1"
+        self.TEXT_MODEL="gpt-3.5-turbo-1106"
+        self.MAIN_ROLE = "Eres un experto en periodismo digital"
+        self.RESPONSE_FORMAT={"type": "json_object"}
+        self.DEFAULT_PROMPT=f'''
+            tu tarea es generar un articulo SEO de minimo 400 palabras y maximo 600,
+            un titulo de maximo 100 caracteres, un resumen de 200 caracteres
+            y cuatro tags separados por comas basado en el siguiente texto:'''
+        self.RESPONSE_OBJECT='''
+            retorna un articulo en formato json que contenga los siguientes campos:
+                "title": ...
+                "sumary": ...
+                "tags": ...
+                "article": ...
+        '''
+
 
     def create_transcription(self,temp_filename):
 
         audio_file= open(temp_filename, "rb")
         transcript = self.client.audio.transcriptions.create(
-        model="whisper-1",
+        model=self.TRANSCRIBE_MODEL,
         file=audio_file,
-        language="es"
+        language=self.SPANISH_LANGUAGE
         )
         return transcript.text
 
-
-    def request_openai(self, message, role="system"):
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": role, "content": message}]
-        )
-        return response["choices"][0]["message"]["content"]
+    def create_article(self, transcription, custom_prompt=None):
+        try:
+            custom_prompt = custom_prompt if custom_prompt is not None else self.DEFAULT_PROMPT
+            prompt = f"""{custom_prompt} ```{transcription}``` ```{self.RESPONSE_OBJECT}```"""
+            response = self.client.chat.completions.create(
+            messages=[
+                    {
+                        "role": "system",
+                        "content": self.MAIN_ROLE
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    }
+                ],
+                model=self.TEXT_MODEL,
+                response_format=self.RESPONSE_FORMAT,
+            )
+            return response.model_dump()['choices'][0]['message']['content']
+        except Exception as e:
+             logging.error(f"Error creating article: {e}")
 
